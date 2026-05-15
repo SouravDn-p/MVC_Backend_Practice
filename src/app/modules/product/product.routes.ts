5c32d05d42f5347c8ad2ce8700146dbd3b68a7ab
SouravDn-p <sdsouravdebnath26@gmail.com>
2026-05-15 11:10:00 +0600

    refreshing all

--- src/app/modules/product/product.routes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { productController } from "./product.controller";
-
-export const router = express.Router();
-
-router.post("/", productController.create);
-router.get("/", productController.getAll);
